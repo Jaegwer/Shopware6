b4723f1c8b96cf66a2c5e2ec9f80092538cebb3a
Ufuk <u.cetin@melexsoft.com>
2023-08-01 17:46:10 +0300

    commit for test

---
+++ custom/plugins2/SwagBasicExample/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	purge: [
+	  './Resources/views/**/*.tpl',
+	  './Resources/**/*.html.twig',
+	],
+	darkMode: false,
+	theme: {
+	  extend: {},
+	},
+	variants: {
+	  extend: {},
+	},
+	plugins: [],
+  }
---
+++ custom/plugins2/SwagBasicExample/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'swag/theme-skeleton',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '69f0b619f37186c4b144ef2324a0303e4fbbcca6',
+        'type' => 'shopware-platform-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'swag/theme-skeleton' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '69f0b619f37186c4b144ef2324a0303e4fbbcca6',
+            'type' => 'shopware-platform-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
